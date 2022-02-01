1c117cbdd22959a140145636d74aeb83ce8d3bcb
puravarora08 <95742586+puravarora08@users.noreply.github.com>
2022-02-01 12:01:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+var first_player_name = document.getElementById("first_player_name").innerHTML;
+var first_player_no = document.getElementById("first_player_no").innerHTML;
+var second_player_name = document.getElementById("second_player_name").innerHTML;
+var second_player_no = document.getElementById("second_player_no").innerHTML;
+var status = document.getElementById("verify_label").textContent;
